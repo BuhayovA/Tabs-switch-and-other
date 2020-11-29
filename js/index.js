6f303e8e0c98364858157ea280e42d5b76120491
Alexsey <bugayow.andrei2014@gmail.com>
2020-11-29 19:00:54 +0200

    fourth  commit

--- js/index.js
+++ js/index.js
@@ -1,25 +1,24 @@
 let tabs = document.querySelectorAll('.tabs-container')
 let output = document.querySelectorAll('.content')
 
-// console.log(tabActive);
-// console.log(output);
-// console.log(tabs);  
-
-
 tabs.forEach( tabsInstance =>  {
    const tabList = tabsInstance.querySelectorAll('.tab')
    let outputs = tabsInstance.querySelectorAll('.content')
-   tabList.forEach((tab, tabIndex) =>{
+   tabsFunction(tabList,outputs)
+} )
+
+
+
+function tabsFunction(tabList,outputs) {
+    tabList.forEach((tab, tabIndex) =>{
+        // if(tabIndex === 0)
+        //     tab.className = 'tab tab_active'
         tab.onclick = () => {
             tabList.forEach(t => t.className = 'tab')
             tab.className = 'tab tab_active'
             outputs.forEach((output, index) => {
-                if(index === tabIndex){
-                    output.className = 'content active'
-                }else {
-                    output.className = 'content'
-                }
+                output.className = +index === tabIndex ? 'content active' : 'content'
             })
         }
    } )
-} )
+}
