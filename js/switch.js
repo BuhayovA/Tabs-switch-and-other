7882b9030ed21257fb3239443bdc129dded2229a
Alexsey <bugayow.andrei2014@gmail.com>
2020-12-07 19:49:16 +0200

    Implement switch-code through Classes

--- js/switch.js
+++ js/switch.js
@@ -1,29 +1,38 @@
 // color()
 
 
+
 // function color () {
-    // let toggle = document.querySelector('.toggle-state')
+//     let toggle = document.querySelector('.toggle-state')
 //     let tabsStyle = document.querySelectorAll('.tabs')
 //     let body = document.querySelector('body')
 //     let switchColor = document.querySelector('.toggle')
 //     let outputsStyle = document.querySelectorAll('.content')
     
 
-    
+//     let toggleState = false;
 
 //     toggle.addEventListener ('click', () => {
 //         tabsStyle.forEach(tab => toggleState === false ? tab.style.background = 'black' : tab.style.background = '#6200ee')
-
-
+//         toggleState === false ? body.style.background = '#121212' : body.style.background = 'white'
+//         toggleState === false ? switchColor.style.background = '#282828' : switchColor.style.background = '#3700b3'
 //         outputsStyle.forEach(outputs => {
 //             toggleState === false ? outputs.style.background = '#282828' : outputs.style.background = 'white'
 //             toggleState === false ? outputs.style.color = 'white' : outputs.style.color = 'black'
 //         })
-//         check()
+//         check()  
 //     })
 
-//     
+//     function check() {
+//         toggleState === true ? toggleState = false : toggleState = true
+//     }
 // }
+
+
+
+
+
+
 let toggleState = false;
 let toggle = document.querySelector('.toggle-state')
 
